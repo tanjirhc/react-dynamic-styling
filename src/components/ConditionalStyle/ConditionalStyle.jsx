497f83b4254e8a86a8b6bae25fd5294076d6bddf
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-09-24 20:53:57 +0600

    styling with object

--- src/components/ConditionalStyle/ConditionalStyle.jsx
+++ src/components/ConditionalStyle/ConditionalStyle.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const ConditionalStyle = () => {
+
+    const commonStyle = {
+        padding: '10px',
+        border: '1px solid black',
+        borderRadius: '5px',
+        margin: '10px 0',
+        backgroundColor: 'lightgray'
+        }
+
   return (
     <div>
       <button>Make it Special</button>
+
+      <div style={commonStyle}>
+        This is a Normal Style!
+      </div>
     </div>
   )
 }
